6afd8d6ea33553b37ede4495f722aaeb5fa53d33
ryancarlson <ryan.t.carlson@cru.org>
2017-12-28 14:48:04 -0500

    Try setting parameter directly

--- TheKeyOAuth2/TheKeyOAuth2Client+PasswordGrant.swift
+++ TheKeyOAuth2/TheKeyOAuth2Client+PasswordGrant.swift
@@ -100,7 +100,7 @@ public extension TheKeyOAuth2Client {
         }
         
         if let thekeyGuid = json["thekey_guid"] as? String {
-            auth.guid = thekeyGuid
+            (auth.parameters as NSDictionary).setValue(thekeyGuid, forKey: "thekeyGuid")
         }
         
         return auth
